c0d0247a24fb4c9f4fb07f070d46f935b7a3fae3
Jesse <jsegall7@gmail.com>
2022-08-12 13:38:12 -0400

    Cleaned up app and added sessions. Dependecies added to package.json

--- app.js
+++ app.js
@@ -1,39 +1,25 @@
-const connection = require("./config/mongoConnection");
-const sitters = require("./data/sitters");
-let sitter1 = undefined;
 const express = require("express");
 const app = express();
 const static = express.static(__dirname + "/public");
 const configRoutes = require("./routes");
-// async function main() {
-//   try {
-//     sitter1 = await sitters.create(
-//       "James",
-//       "Kirk",
-//       "jkirk@gmail.com",
-//       "jimmyK",
-//       "will be a hashed pw",
-//       ["ids of dogs sat"],
-//       "picture.jpg",
-//       "50$",
-//       "3 days"
-//     );
-//     console.log(sitter1);
-//   } catch (e) {
-//     console.log(e);
-//   }
-//   await connection.closeConnection();
-//   console.log("Connection has been closed.");
-// }
-
-// main();
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 
 app.use("/public", static);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(
+  session({
+    name: "AuthCookie",
+    secret: "some secret string!",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 configRoutes(app);
 
