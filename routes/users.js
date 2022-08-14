54ea10d10913f77df61aa518fe0934c65ad8d6bb
Jesse <jsegall7@gmail.com>
2022-08-14 16:06:21 -0400

    Fixed spelling issue

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const data = require("../data");
 const salt = 8;
 const ownerData = data.owners;
-const sittersData = data.sitters;
+const sitterData = data.sitters;
 
 router.get("/", async (req, res) => {
   // Just for testing purposes
@@ -51,6 +51,48 @@ router.post("/registerOwner", async (req, res) => {
       hash
     );
     req.session.userId = newOwner._id.toHexString();
+    return res.resdirect("/dashboard"); // Should redirect to either home page or straight to their dashboard after registration
+  } catch (e) {
+    return res.status(404).render(); // TODO: Need to add our homepage to render if error
+  }
+});
+
+router.post("/registerSitter", async (req, res) => {
+  const { user_name, first_name, last_name, email, password, password_confm } =
+    req.body;
+
+  try {
+    if (!user_name) {
+      throw `Please enter a Username.`;
+    }
+    if (!first_name) {
+      throw `Please enter your First Name.`;
+    }
+    if (!last_name) {
+      throw `Please enter your Last Name.`;
+    }
+    if (!email) {
+      throw `Please enter your email address.`;
+    }
+    if (!password) {
+      throw `Please enter a password.`;
+    }
+    if (!password_confm) {
+      throw `Please confirm your password.`;
+    }
+
+    if (password !== password_confm) {
+      throw `Passwords do not match, please try again.`;
+    }
+    const hash = await bcrypt.hash(password, salt);
+    const newSitter = await sitterData.addSitter(
+      first_name,
+      last_name,
+      email,
+      user_name,
+      hash
+    );
+    req.session.userId = newSitter._id.toHexString();
     return res.resdirect("/dashboard"); // Should redirect to either home page or straight to their dashboard after registration
   } catch (e) {
     return res.status(404).render(); // TODO: Need to add our homepage to render if error
