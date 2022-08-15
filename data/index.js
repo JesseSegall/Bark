95de6a2a7eaad3f8dd6604d50a9e8e4e31899242
Jesse <jsegall7@gmail.com>
2022-08-15 17:52:59 -0400

    deleted collections not in use

--- data/index.js
+++ data/index.js
@@ -1,15 +1,13 @@
-const sittersData = require("./sitters");
 const reviewData = require("./reviews");
 const dogsData = require("./dogs");
-const ownersData = require("./owners");
+
 const requestsData = require("./requests");
 const usersData = require("./users");
 
 module.exports = {
-  sitters: sittersData,
   reviews: reviewData,
   dogs: dogsData,
-  owners: ownersData,
+
   requests: requestsData,
   users: usersData,
 };
