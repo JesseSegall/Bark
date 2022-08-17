e840446332c9a31878c3550f17d00e9cefc1ba29
Jesse <jsegall7@gmail.com>
2022-08-17 16:29:11 -0400

    added sitter route to index

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const usersRoutes = require("./users");
+const availSitters = require("./availableSitters");
 
 const constructor = (app) => {
-  app.use("/", usersRoutes);
-  // res.redirect("/");
+	app.use("/", usersRoutes);
+	app.use("/sitters", availSitters);
 
-  app.use("*", (req, res) => {
-    res.sendStatus(404);
-  });
+	app.use("*", (req, res) => {
+		res.sendStatus(404).json({ error: "Not found" });
+	});
 };
 
 module.exports = constructor;
