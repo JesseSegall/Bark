51b552548c12abc46e63898fdec3eadd81bf41a1
Bhavin Mistry <bmistry1@stevens.edu>
2022-08-18 23:06:39 -0400

    Added owners page route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const usersRoutes = require("./users");
 const availSitters = require("./availableSitters");
 const ownerReviewRoutes = require("./ownerReview");
+const owners = require("./ownerProfiles");
 
 const constructor = (app) => {
 	app.use("/", usersRoutes);
 	app.use("/sitters", availSitters);
 	app.use("/ownerReview", ownerReviewRoutes);
+	app.use("/owners", owners);
 
 	app.use("*", (req, res) => {
 		res.sendStatus(404).json({ error: "Not found" });
