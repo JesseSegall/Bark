cac02ae06e919372366feab0a9860a3b09935b1e
Bhavin Mistry <bmistry1@stevens.edu>
2022-08-22 10:59:33 -0400

    updated ownerProfiles route

--- routes/ownerProfiles.js
+++ routes/ownerProfiles.js
@@ -14,4 +14,14 @@ router.get("/", async (req, res) => {
 	}
 });
 
+router.get("/:id", async (req, res) => {
+	try {
+		const ownerData = await users.getOwner(req.params.id);
+		console.log(ownerData);
+		return res.render('partials/ownerProfile', { owner: ownerData });
+	} catch (error) {
+		res.status(500).json({ error: error });
+	}
+});
+
 module.exports = router;
