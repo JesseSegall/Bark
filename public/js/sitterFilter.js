c8f879266b7af060f14529b21ccbcd2016e44c26
Bhavin Mistry <bmistry1@stevens.edu>
2022-08-23 20:42:58 -0400

    fixed difficult dog filter

--- public/js/sitterFilter.js
+++ public/js/sitterFilter.js
@@ -169,6 +169,7 @@
 
 	dogMedium.on('click', function (e) {
 		e.preventDefault();
+		console.log("Medium Click");
 		unorderedSittersDiv.attr('hidden', true);
 		priceHighDiv.attr('hidden', true);
 		priceLowDiv.attr('hidden', true);
@@ -206,7 +207,7 @@
 
 	dogLarge.on('click', function (e) {
 		e.preventDefault();
-		console.log('I clicked');
+		console.log('I clicked Large');
 		unorderedSittersDiv.attr('hidden', true);
 		priceHighDiv.attr('hidden', true);
 		priceLowDiv.attr('hidden', true);
@@ -240,42 +241,42 @@
 			}
 			dogLargeDiv.attr('hidden', false);
 		});
+	});
 
-		difficultDog.on('click', function (e) {
-			e.preventDefault();
-			console.log('I have been clicked');
-			unorderedSittersDiv.attr('hidden', true);
-			priceHighDiv.attr('hidden', true);
-			priceLowDiv.attr('hidden', true);
-			dogSmallDiv.attr('hidden', true);
-			dogMediumDiv.attr('hidden', true);
-			dogLargeDiv.attr('hidden', true);
-			dogDifficultDiv.attr('hidden', false);
+	difficultDog.on('click', function (e) {
+		e.preventDefault();
+		console.log('difficult: I have been clicked');
+		unorderedSittersDiv.attr('hidden', true);
+		priceHighDiv.attr('hidden', true);
+		priceLowDiv.attr('hidden', true);
+		dogSmallDiv.attr('hidden', true);
+		dogMediumDiv.attr('hidden', true);
+		dogLargeDiv.attr('hidden', true);
+		dogDifficultDiv.attr('hidden', false);
 
-			let req = {
-				method: 'GET',
-				url: 'http://localhost:3000/sitters/filterDogsDifficult',
-				contentType: 'json',
-			};
+		let req = {
+			method: 'GET',
+			url: 'http://localhost:3000/sitters/filterDogsDifficult',
+			contentType: 'json',
+		};
 
-			$.ajax(req).then((res) => {
-				for (i = 0; i < res.length; i++) {
-					dogDiffCard.append(
-						`
-						<div id="card-data" class="col-auto mb-3">
-						<div class="card" style="width: 300px;">
-							<img src="public/image/no_image.jpeg" class="card-img-top" alt="Profile Picture" />
-							<div class="card-body text-center">
-								<h5 class="card-title">${res[i].firstName}</h5>
-								<p class="card-text">${res[i].price}</p>
-								<p class="card-text">${res[i].rating}</p>
-								<a href="/searchSitter/${res[i]._id}" class="btn btn-primary">View Profile</a>
-							</div>
+		$.ajax(req).then((res) => {
+			for (i = 0; i < res.length; i++) {
+				dogDiffCard.append(
+					`
+					<div id="card-data" class="col-auto mb-3">
+					<div class="card" style="width: 300px;">
+						<img src="public/image/no_image.jpeg" class="card-img-top" alt="Profile Picture" />
+						<div class="card-body text-center">
+							<h5 class="card-title">${res[i].firstName}</h5>
+							<p class="card-text">${res[i].price}</p>
+							<p class="card-text">${res[i].rating}</p>
+							<a href="/searchSitter/${res[i]._id}" class="btn btn-primary">View Profile</a>
 						</div>
-					</div>`
-					);
-				}
-			});
+					</div>
+				</div>`
+				);
+			}
 		});
 	});
 })(window.jQuery);
