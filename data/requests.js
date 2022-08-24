1aaa47b01b882dc1b8eaca3084bd9d90ee0aa6e7
Jesse Segall <jsegall7@gmail.com>
2022-08-23 22:40:44 -0700

    quotes

--- data/requests.js
+++ data/requests.js
@@ -1,18 +1,18 @@
-const mongoCollections = require("../config/mongoCollections");
+const mongoCollections = require('../config/mongoCollections');
 const requests = mongoCollections.requests;
 const users = mongoCollections.users;
 //const sitters = mongoCollections.sitters;
-const { ObjectId } = require("mongodb");
+const { ObjectId } = require('mongodb');
 
 let exportedMethods = {
-  async addRequest(ownerId, sitterId, requestText, dogId) {
-    let newRequest = {
-      ownerId: ownerId,
-      sitterId: sitterId,
-      requestText: requestText,
-      dogId: dogId,
+	async addRequest(ownerId, sitterId, requestText, dogId) {
+		let newRequest = {
+			ownerId: ownerId,
+			sitterId: sitterId,
+			requestText: requestText,
+			dogId: dogId,
 
-      /*TODO
+			/*TODO
       message:{
         text field describing what the request is (Date, etc)
       }
@@ -22,30 +22,30 @@ let exportedMethods = {
          - Declined (Sitter declined)
       }
       */
-    };
+		};
 
-    const usersCollection = await users();
-    /*
+		const usersCollection = await users();
+		/*
     const sittersCollection = await sitters();
     */
-    const requestsCollection = await requests();
-    
-    const insertRequest = await requestsCollection.insertOne(newRequest);
-    if (insertRequest.insertedCount === 0) throw "Could not insert Request";
-    const requestId = newRequest._id;
-    console.log(requestId);
-    const updateRequestsArray = await usersCollection.updateOne(
-      { _id: ObjectId(sitterId) },
-      { $push: { requests: requestId } }
-    );
-    return await this.getRequest(insertRequest.insertedId.toString());
-  },
+		const requestsCollection = await requests();
 
-  async getRequest(id) {
-    const requestsCollection = await requests();
-    const request = await requestsCollection.findOne({ _id: ObjectId(id) });
-    return request;
-  },
+		const insertRequest = await requestsCollection.insertOne(newRequest);
+		if (insertRequest.insertedCount === 0) throw 'Could not insert Request';
+		const requestId = newRequest._id.toString();
+		console.log(requestId);
+		const updateRequestsArray = await usersCollection.updateOne(
+			{ _id: ObjectId(sitterId) },
+			{ $push: { requests: requestId } }
+		);
+		return await this.getRequest(insertRequest.insertedId.toString());
+	},
+
+	async getRequest(id) {
+		const requestsCollection = await requests();
+		const request = await requestsCollection.findOne({ _id: ObjectId(id) });
+		return request;
+	},
 };
 
 module.exports = exportedMethods;
