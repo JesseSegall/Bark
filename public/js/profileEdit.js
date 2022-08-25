d08057fa8a5511ad0cfde92be388caca1b246092
Terrance Peters <terrancejpeters@gmail.com>
2022-08-24 21:45:55 -0400

    various try wraps

--- public/js/profileEdit.js
+++ public/js/profileEdit.js
@@ -25,14 +25,21 @@
 
 	$.ajax(req).then((res) => {
 		console.log(res);
-		//TODO: Error handling for all user inputs make sure they are strings etc
+		//TO done: Error handling for all user inputs make sure they are strings etc
+		if (!res.user.firstName || typeof(res.user.firstName) != 'string') throw "bad input!"
 		firstName.val(res.user.firstName);
+		if (!res.user.lastName || typeof(res.user.lastName) != 'string') throw "bad input!"
 		lastName.val(res.user.lastName);
+		if (!res.user.address.street || typeof(res.user.address.street) != 'string') throw "bad input!"
 		street.val(res.user.address.street);
+		if (!res.user.address.city || typeof(res.user.address.city) != 'string') throw "bad input!"
 		city.val(res.user.address.city);
+		if (!res.user.address.state || typeof(res.user.address.state) != 'string') throw "bad input!"
 		state.val(res.user.address.state);
+		if (!res.user.address.zip || typeof(res.user.address.zip) != 'string') throw "bad input!"
 		zip.val(res.user.address.zip);
 		country.val(res.user.address.country);
+		if (!res.user.address.country || typeof(res.user.address.country) != 'string') throw "bad input!"
 		if (sitterProfileForm.length > 0) {
 			smallDog.val(res.user.price.smallDog);
 			mediumDog.val(res.user.price.mediumDog);
